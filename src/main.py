69d328dfc86fa64b4fff7899a509564e1b592438
krystallinekim <jskim11070@gmail.com>
2025-10-14 21:21:04 +0900

    실행 결과 저장 및 예시 데이터 실행 결과

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 from dotenv import load_dotenv
 from graph.builder import build_graph
+import pandas as pd
 
 load_dotenv(override=True)
 
 def main():
-    input_id = input('검색 데이터의 ID')
-    input_category = input('article / dataset')
+    
+    # input_id = 'JAKO200411922932805'
+    # input_category = 'article'
+
+    # input_id = '37c0f3d51a130211fe55fe6019cc7914'
+    # input_category = 'dataset'
+
+    input_id = input('[검색 데이터의 ID]\n')
+    input_category = input('article / dataset\n')
         
     graph = build_graph()
     
@@ -15,6 +23,8 @@ def main():
     })
 
     print(res['result_df'])
+    df = res['result_df']
+    df.to_csv('./src/analysis_result.csv', index=False, encoding='utf-8')
 
 if __name__ == "__main__":
     main()
